88fed24ac6c2358a7390f89666a5a7d902870ce3
ValeryPiniazikITechArt <36072114+ValeryPiniazikITechArt@users.noreply.github.com>
2018-02-09 17:56:27 +0300

    Removed AtFilmDetailsPage.js

--- Steps/AtFilmDetailsPage.js
+++
@@ -1,8 +0,0 @@
-var FilmDetailsPage=require('../PageObjects/FilmDetailsPage');
-class AtFilmDetailsPage{
-      GetTitle(){
-          return FilmDetailsPage.FilmTitle.getText();
-      }
-           
-}
-module.exports=new AtFilmDetailsPage();
